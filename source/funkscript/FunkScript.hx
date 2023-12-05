d4a807c861a2203dabaeee98ffe37a31a53b2f84
Blitz <93739827+blitzdotcs@users.noreply.github.com>
2023-12-05 11:28:10 -0500

    Revert "uhhh"
    This reverts commit c2114250a1c0ceeaac285f3ed0776c7688627032.

--- source/funkscript/FunkScript.hx
+++ source/funkscript/FunkScript.hx
@@ -82,7 +82,7 @@ class FunkScript {
 		}
 	}
 
-	public function loademptyScript()
+	public function loademptyScript(file:String)
 	{
 		var emptyshithxpath:String = Paths.gettempSongScript(file);
 		if (sys.FileSystem.exists(emptyshithxpath))
