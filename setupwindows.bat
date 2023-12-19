5612bf1f9f385cd5a64fe5e67aa2969df1309484
Blitz <93739827+blitzdotcs@users.noreply.github.com>
2023-12-19 00:49:01 -0500

    pluh

---
+++ setupwindows.bat
@@ -0,0 +1,6 @@
+          haxelib install hmm
+          haxelib run hmm install
+          haxelib set flixel 4.11.0
+          haxelib set flixel-tools 1.5.1
+          haxelib set flixel-ui 2.5.0
+          haxelib set flixel-addons 2.11.0
